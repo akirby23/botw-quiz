e9a9b5e06db4ff1b8071d563b72edc71d54e1cc6
akirby23 <>
2023-07-08 20:48:01 +0000

    Nest functions within main() function

--- run.py
+++ run.py
@@ -12,6 +12,18 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('botw_quiz')
 
+data = SHEET.worksheet("questions_and_answers")
+questions_dict = data.get_all_records()
+
+for x in questions_dict: 
+    question = x['question']
+    option_a = x['option_a']
+    option_b = x['option_b']
+    option_c = x['option_c']
+    option_d = x['option_d']
+    correct_answer = x['correct_answer']
+
+
 def get_username():
     print("Greetings, Hylian!\n")
 
@@ -26,16 +38,6 @@ def load_main_menu():
     print("3. Exit Quiz")
     menu_choice = input("Enter your choice (1-3): ")
 
-data = SHEET.worksheet("questions_and_answers")
-questions_dict = data.get_all_records()
-
-for x in questions_dict: 
-    question = x['question']
-    option_a = x['option_a']
-    option_b = x['option_b']
-    option_c = x['option_c']
-    option_d = x['option_d']
-    correct_answer = x['correct_answer']
 
 def run_quiz():
     print(question)
@@ -45,5 +47,9 @@ def run_quiz():
     print(option_d)
     user_answer = input("Your answer: ")
 
-run_quiz()
+
+def main():
+    get_username()
+    load_main_menu()
+    run_quiz()
 
