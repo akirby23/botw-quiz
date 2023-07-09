7098c299741a8845fb5c188c256901d8f074a1d5
akirby23 <>
2023-07-09 15:38:52 +0000

    Add while loop to load_main_menu() function to validate input

--- run.py
+++ run.py
@@ -34,20 +34,21 @@ def get_username():
     print(f"Welcome, {username}!\n")
 
 def load_main_menu():
+    menu_choice = 0
     print("Please select one of the following options:\n")
     print("1. Start Quiz")
     print("2. Scoreboard")
     print("3. Exit Quiz")
-    try:
-        menu_choice = input("Enter your choice (1-3): ")
-        if menu_choice == 1:
-            run_quiz()
-        elif menu_choice == 2:
-            print("Load scoreboard - placeholder")
-        elif menu_choice == 3:
-            print("Exiting..")
-    except ValueError as e:
-        print("Invalid input: {e}, please enter a number from 1-3.")
+    while True:
+        try:
+            menu_choice = int(input("Enter your choice (1-3): "))
+        except ValueError:
+            print("Invalid input. Please enter a number between 1-3.")
+            continue
+        if menu_choice >= 1 and menu_choice <= 3:
+            continue
+        else:
+            print("Invalid input. Please enter a number between 1-3.")
 
 
 
